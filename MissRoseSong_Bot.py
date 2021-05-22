51f487105514be09c867846efe4dc4c808dd9671
Aswin-Raj-TG <82755354+Aswin-Raj-TG@users.noreply.github.com>
2021-05-22 21:24:44 +0530

    Update MissRoseSong_Bot.py

--- MissRoseSong_Bot.py
+++ MissRoseSong_Bot.py
@@ -42,8 +42,8 @@ def start(client, message):
     ]]
 
 
-@Client.on_message(filters.command(['mt', 'song']) & filters.chat(@mt_music_24))
-def a(client, message):
+@Client.on_message(filters.command(["mt", "song"]) & filters.chat(@mt_music_24))
+def song_dl(client, message):
     query = ''
     for i in message.command[1:]:
         query += ' ' + str(i)
