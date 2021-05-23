2a97b478c4762c77cbb52e9f99a3f598e19fb51d
Aswin-Raj-TG <82755354+Aswin-Raj-TG@users.noreply.github.com>
2021-05-23 12:21:55 +0530

    Update MissRoseSong_Bot.py

--- MissRoseSong_Bot.py
+++ MissRoseSong_Bot.py
@@ -28,7 +28,7 @@ def time_to_seconds(time):
 ## Commands --------------------------------
 @bot.on_message(filters.command(['start']))
 def start(client, message):
-    amegh ="**Hi {},\n\nI Can Download Song's From YouTube. But I Can Only Work In My Music Group.**".format(message.from_user.mention),
+    amegh ='**Hi {message.from_user.mention} . \n\nI Can Download Song's From YouTube. But I Can Only Work In My Music Group.**'
     message.reply_text(
         text=amegh, 
         quote=False,
