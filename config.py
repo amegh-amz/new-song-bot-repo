7245445de0cb24585ec66d81517d4ae329616d9b
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-06-24 13:25:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config:
-    BOT_TOKEN = "1514003548:AAGVuwyeiXFpnb9uvl_-pYbQlI98bAbrnQo" 
-    API_ID = 2207333
-    API_HASH = "bc9f06a7258ae2049b426a46c0f27174" 
+    BOT_TOKEN = "1836226590:AAHukWnpGcrUO18_ytOeHro8qZkLI7mON8k" 
+    API_ID = 6624445
+    API_HASH = "d7bf4c140d85cf1338afb2a5a27e7cb8
+    " 
     
